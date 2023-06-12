461e5bcdbf352ea523db926073e78016929da89c
Arora, Karan <nsit_karan@yahoo.com>
2023-06-12 23:54:23 +0530

    isAnagram for 2 strings

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,9 +6,18 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
+let sortString = (str) => {
+  return str.split("").sort().join("");
+};
+  
 function isAnagram(str1, str2) {
+  str1 = sortString(str1.toLowerCase());
+  str2 = sortString(str2.toLowerCase());
 
+  if (str1 == str2) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isAnagram;
