63295d40aebd491a5377d46fb3fb9dee1945937a
waternsky <playgamesroyale2@gmail.com>
2023-06-13 00:48:49 +0530

    anagram problem finished all test cases passed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,36 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function charCounts(str) {
+    var charMap = new Map();
+    for (const ch of str) {
+        charMap.set(ch, 0);
+    }
+    for (const ch of str) {
+        var count = charMap.get(ch);
+        charMap.set(ch, count + 1);
+    }
+    return charMap;
+}
 
+function isAnagram(str1, str2) {
+    var ans = true;
+    var charMapstr1 = charCounts(str1.toLowerCase());
+    var charMapstr2 = charCounts(str2.toLowerCase());
+    if (charMapstr1.size != charMapstr2.size) {
+        return false;
+    }
+    for ([key, value] of charMapstr1) {
+        if (!charMapstr2.has(key)) {
+            ans = false;
+            break;
+        }
+        if (charMapstr2.get(key) != value) {
+            ans = false;
+            break;
+        }
+    }
+    return ans;
 }
 
 module.exports = isAnagram;
