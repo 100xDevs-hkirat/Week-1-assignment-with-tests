a0942c6897c1169b041bec699eb366ff0a4c6f44
Dhruv Gupta <96371303+incheif@users.noreply.github.com>
2023-06-12 21:52:44 +0000

    Anagram problem

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,29 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  let freqstr1 = Array(256).fill(0); // Consider a larger range
+  let n1 = str1.length;
 
+  for (let i = 0; i < n1; i++) {
+    freqstr1[str1.charCodeAt(i)]++;
+  }
+
+  let freqstr2 = Array(256).fill(0); // Consider a larger range
+  let n2 = str2.length;
+
+  for (let i = 0; i < n2; i++) {
+    freqstr2[str2.charCodeAt(i)]++;
+  }
+
+  // Compare the contents of the arrays
+  for (let i = 0; i < 256; i++) { // Consider a larger range
+    if (freqstr1[i] !== freqstr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
-
 module.exports = isAnagram;
