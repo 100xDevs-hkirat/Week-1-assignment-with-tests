68a4f91da51efc792dc8e1e347d6b2935de85f68
Anuj Mittal <68385588+anujmittal1990@users.noreply.github.com>
2023-06-12 23:01:30 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,69 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryTotal = {};
+  const result = [];
+
+  for (const i of transactions) {
+    //Destructuring Object for Category and Price
+    const { category, price } = i;
+
+    //checking if category already created in object
+    if (category in categoryTotal) {
+      //adding expense to the existing category price
+      categoryTotal[category] += price;
+    } else {
+      //creating new category and adding price
+      categoryTotal[category] = price;
+    }
+  }
+
+  //displaying categorywise total expenditure
+  for (const category in categoryTotal) {
+    const totalSpent = categoryTotal[category];
+    const categoryObj = { [category]: totalSpent };
+    result.push(categoryObj);
+  }
+
+  return result;
 }
+
+// Sample Categories
+const transactions = [
+  {
+    itemName: "Laptop",
+    category: "Category1",
+    price: 1000000,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Desktop",
+    category: "Category1",
+    price: 60000,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Television",
+    category: "Category2",
+    price: 27000,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Fridge",
+    category: "Category2",
+    price: 28000,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Mixer Juicer",
+    category: "Category2",
+    price: 8000,
+    timestamp: Date.now(),
+  },
+];
+
+const totalSpentByCategory = calculateTotalSpentByCategory(transactions);
+console.log(totalSpentByCategory);
+
 
 module.exports = calculateTotalSpentByCategory;
