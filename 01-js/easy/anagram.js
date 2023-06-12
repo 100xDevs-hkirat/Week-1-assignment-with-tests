e03b92017fbaa61434016276387067e4966f1355
Swastik <swastikpanda32@gmail.com>
2023-06-12 22:39:50 +0530

    Updated anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -9,6 +9,24 @@
 
 function isAnagram(str1, str2) {
 
+  // convert them to lowercase incaase of case insensitivity
+
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  // split the words into array , sort the words in ascending and join them back into strings
+
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  // compare both the sorted words to check if they match
+
+  return sortedStr1 === sortedStr2;
+
 }
+
+console.log(isAnagram('rasp','spar'))
+console.log(isAnagram('listen','silent'))
+console.log(isAnagram('listen','silents'))
 
 module.exports = isAnagram;
