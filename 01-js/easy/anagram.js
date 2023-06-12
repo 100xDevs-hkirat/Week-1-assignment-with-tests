9fb81a2eef300429bd12a9938a8ebeee1a412e55
Lokesh <lokeshmarwal007@gmail.com>
2023-06-12 22:50:54 +0530

    solved anagram problem with all test cases passed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -9,6 +9,17 @@
 
 function isAnagram(str1, str2) {
 
+  const correctstr1 = str1.toLowerCase();
+  const correctstr2 = str2.toLowerCase();
+  
+  // split('') is used to split the cleaned strings into an array of individual characters.
+  // sort() is used to sort the array of characters in lexicographic order.
+  // join('') is used to join the sorted array of characters back into a single string.
+
+  const sortedstr1 = correctstr1.split('').sort().join('');
+  const sortedstr2 = correctstr2.split('').sort().join('');
+
+  return (sortedstr1 === sortedstr2)
 }
 
 module.exports = isAnagram;
