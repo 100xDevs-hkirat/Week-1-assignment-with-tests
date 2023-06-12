9c3f7db3e716aa7a8d9b0276e15836a99a55d469
Mohandeep <mohandeep2002@gmail.com>
2023-06-13 00:38:47 +0530

    anagram.js completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,36 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
+    console.log("false");
+    return false;
+  }
 
+  var dict1 = {};
+  var dict2 = {};
+  for (var i = 0; i < str1.length; i++) {
+    var val = str1[i].toLowerCase();
+    if (val in dict1) {
+      dict1[val]++;
+    } else {
+      dict1[val] = 1;
+    }
+  }
+  for (var i = 0; i < str2.length; i++) {
+    var val = str2[i].toLowerCase();
+    if (val in dict2) {
+      dict2[val]++;
+    } else {
+      dict2[val] = 1;
+    }
+  }
+  for (var key in dict1) {
+    if (key in dict2) {
+      if (dict2[key] != dict1[key]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
-
 module.exports = isAnagram;
