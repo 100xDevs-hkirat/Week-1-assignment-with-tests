f699c88ad22cd95add86ebd5844e5275049ada11
Naman Gupta <namanitsofficial@gmail.com>
2023-06-13 04:09:24 +0530

    commit 1

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,38 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function isAlphabetic(char) {
+  return (char >= "a" && char <= "z") || (char >= "A" && char <= "Z");
+}
 
+function isAnagram(str1, str2) {
+  const str1FrequencyMap = {};
+  const str2FrequencyMap = {};
+
+  for (const ch of str1) {
+    let char1 = ch;
+    if (isAlphabetic(ch)) {
+      char1 = char1.toLowerCase();
+    }
+
+    if (str1FrequencyMap[char1]) str1FrequencyMap[char1]++;
+    else str1FrequencyMap[char1] = 1;
+  }
+  for (const ch of str2) {
+    let char2 = ch;
+    if (isAlphabetic(ch)) {
+      char2 = char2.toLowerCase();
+    }
+    if (str2FrequencyMap[char2]) str2FrequencyMap[char2]++;
+    else str2FrequencyMap[char2] = 1;
+  }
+
+  return (
+    str1.length === str2.length &&
+    Object.keys(str1FrequencyMap).every(
+      (key) => str1FrequencyMap[key] === str2FrequencyMap[key]
+    )
+  );
 }
 
 module.exports = isAnagram;
