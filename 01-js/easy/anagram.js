af1153e1bca7f86eb7b5ea90c7fbf08638cb28ec
IbrahimVK <ib@logrythmic.com>
2023-06-12 21:49:50 +0530

    01 - anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,24 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let len1 = str1.length;
+  let len2 = str2.length;
+  if (len1 !== len2) {
+    return false;
+  }
+  let a = str1
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join(""); 
+  let b = str2
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join("");
+  return a === b;
 }
 
 module.exports = isAnagram;
