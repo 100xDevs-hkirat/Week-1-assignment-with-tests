aefb51278e4d91c34c3dd3207f6686b5c2c6ab46
esakkiraja100116 <esakkiraja100116@gmail.com>
2023-06-14 01:44:55 +0530

    calculator class completed

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,37 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
+  add(num) {
+    this.result += num;
+  }
+  subtract(num) {
+    this.result -= num;
+  }
+  multiply(num) {
+    this.result *= num;
+  }
+  divide(num) {
+    if (num === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    this.result /= num;
+  }
+  clear() {
+    this.result = 0;
+  }
+  getResult() {
+    return this.result;
+  }
+  calculate(str) {
+    const response = eval(str);
+    if (response === Infinity) throw new Error("Cannot divide by zero");
+    this.result = response;
+  }
+
+}
 
 module.exports = Calculator;
