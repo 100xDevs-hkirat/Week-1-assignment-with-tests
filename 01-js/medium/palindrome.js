0dd9ac146aa97f2a4a07a4554601564f77d9cd04
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-06-13 15:48:45 +0530

    ✅ String Palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,6 +7,18 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
   return true;
 }
 
