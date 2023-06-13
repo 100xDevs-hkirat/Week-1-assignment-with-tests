d270abdb2bd00e5cf589a7b8daf5236e3aa458a8
mani <111manikanta.v@gmail.com>
2023-06-13 06:08:58 +0530

    times question is solved

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,15 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+
+    let startTime = performance.now();
+
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    let endTime = performance.now()
+    return (endTime - startTime) / 1000
+
 }
