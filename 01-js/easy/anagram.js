9eeca184700e9ea3574caca3768cfa3f73e3aa31
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-06-14 00:53:33 +0530

    anagram is completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,6 +8,14 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false
+  } else {
+    const sortedStr1 = str1.toLowerCase().split('').sort().join('')
+    const sortedStr2 = str2.toLowerCase().split('').sort().join('')
+    return sortedStr1 === sortedStr2;
+
+  }
 
 }
 
