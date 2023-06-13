a9fbda33c9b63997f8dd9f2f7e907b1e2ed2ea5e
Arora, Karan <nsit_karan@yahoo.com>
2023-06-13 17:37:48 +0530

    return true is no is palindrome or not

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -6,8 +6,49 @@
   - `npm run test-palindrome`
 */
 
+/**
+ * Remove all the characters like !,.?spaces
+ */
+function sanitizeStrings(str) {
+  let sanitizedStr = "";
+  let strVector = str.split("");
+  for (let ch of strVector) {
+    if (ch != " " && ch != "!" && ch != "&" && ch != "!" && ch != '?' && ch != ',' && ch != '.') {
+      sanitizedStr = sanitizedStr + ch;
+    }
+  }
+
+  return sanitizedStr;
+}
+
+function reverseString(str) {
+  str = str.split("").reverse().join("");
+  return str;
+}
+/**
+ * 
+ * Do the following:
+ * Sanitize the string
+ * trim start/end
+ * reverse the string
+ * 
+ * compare reversed == str and return
+ */
 function isPalindrome(str) {
-  return true;
+
+  str = sanitizeStrings(str);
+  str = str.trimStart().trimEnd();
+  str = str.toLowerCase()
+  reversedStr = reverseString(str);
+
+  console.log("Sanitzed string is " + str);
+  console.log("reverses string is " + reversedStr);
+  if (str == reversedStr) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isPalindrome;
+
+console.log(isPalindrome("Eva, can I see bees in a cave?"));
