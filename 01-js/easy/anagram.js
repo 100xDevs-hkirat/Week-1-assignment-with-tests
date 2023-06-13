ba8c354b149dfdc044babe9db32bfd405fe64b92
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2023-06-13 17:03:36 +0530

    ✅ anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,46 @@
 */
 
 function isAnagram(str1, str2) {
-
+  const sorted1= str1.toLowerCase().split("").sort().join("");
+  const sorted2 = str2.toLowerCase().split('').sort().join('');
+  if (sorted1 !== sorted2) {
+    return false;
+  }
+  return true;
 }
+
+// function isAnagram(str1, str2){
+//   str1 = str1.toLowerCase();
+//   str2 = str2.toLowerCase();
+
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
+
+//   // Create character frequency maps for both strings
+//   const charCount1 = {};
+//   const charCount2 = {};
+
+//   // Count the occurrences of each character in str1
+//   for (let i = 0; i < str1.length; i++) {
+//     const char = str1[i];
+//     charCount1[char] = (charCount1[char] || 0) + 1;
+//   }
+
+//   // Count the occurrences of each character in str2
+//   for (let i = 0; i < str2.length; i++) {
+//     const char = str2[i];
+//     charCount2[char] = (charCount2[char] || 0) + 1;
+//   }
+
+//   // Check if the character frequency maps are equal
+//   for (const char in charCount1) {
+//     if (charCount1[char] !== charCount2[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
 
 module.exports = isAnagram;
