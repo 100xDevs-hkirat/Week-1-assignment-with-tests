dc8110d4c0857fe3d523cebb7534fa5a09188ab1
Sagar Singh Negi <sagarssn23@gmail.com>
2023-06-13 11:27:39 +0530

    Assignment Week 1.2-hard

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,25 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, seconds * 1000);
+    });
 }
+
+
+async function main() {
+    await sleep(10).then(() => {
+        console.log("yaa now you can run")
+    });
+
+
+    console.log("start")
+    console.log("stop")
+    console.log("do something")
+}
+
+main();
+
