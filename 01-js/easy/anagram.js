3e70cb1162fd1cf37fc13fe74f7ba4be83fba23a
manosatpathy <manosatpathy@gmail.com>
2023-06-13 21:43:09 +0530

    completing easy assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  let string1 = str1.toLowerCase().split("").sort().join("");
+  let string2 = str2.toLowerCase().split("").sort().join("");
+  return string1 == string2;
 }
+
+isAnagram('toDd', 'DDOt')
 
 module.exports = isAnagram;
