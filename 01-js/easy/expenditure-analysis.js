71b20db4dd3a0310eea24c83c720e08ce91e4ac3
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-06-14 23:23:20 +0530

    completed easy

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,17 +14,19 @@ function calculateTotalSpentByCategory(transactions) {
   for (const transaction of transactions) {
     const { category, price } = transaction;
     if (category in categoryMap) {
-      categoryMap[category].totalSpent += price;
+      categoryMap[category] += price;
     } else {
-      categoryMap[category] = {"totalSpent": price};
+      categoryMap[category] =  price;
     }
   }
 
-  const result = Object.entries(categoryMap).map(([category, total]) => {
-    return { [category]: total };
-  });
+  
+  let arr = []
+  for(category in categoryMap){
+    arr.push({category:category,totalSpent:categoryMap[category]})
+  }
+  return arr;
 
-  return result;
 }
 
 
