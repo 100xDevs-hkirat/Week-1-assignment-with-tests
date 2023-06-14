743c56d467efef0d42fef20be97818674c3fede9
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-06-14 18:00:18 +0530

    Solved Hard(Promises) -all

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,20 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
 
+const getCurrentTime = () => {
+    const timestamp = Date.now() 
+    const date = new Date(timestamp)
+    return `${date.getHours()}h:${date.getMinutes()}m:${date.getSeconds()}s`
 }
+
+const sleep = (seconds) => {
+    const startTimeInMilliSecond = Date.now() ;
+    while (Date.now() - startTimeInMilliSecond < seconds*1000) { 
+        // Do nothing, busy waiting , JS thread busy during the delay
+    }
+}
+
+console.log(`Execution Started : ${getCurrentTime()}`)
+sleep(2) ;
+console.log(`Delayed, Got Thread back at : ${getCurrentTime()}`)
--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -1,22 +1,60 @@
 /*
- * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
- * Write a function that sequentially calls all 3 of these functions in order.
- * Print out the time it takes to complete the entire operation.
- * Compare it with the results from 3-promise-all.js
- */
+    * Write 3 different functions that return promises that resolve after 1, 2, and 3 seconds respectively.
+    * Write a function that sequentially calls all 3 of these functions in order.
+    * Print out the time it takes to complete the entire operation.
+    * Compare it with the results from 3-promise-all.js
+*/
 
+
+/*
+    Promise.all : This allows multiple asynchronous (Promises) operations to run in parallel. 
+                  Suitable when you have multiple independent asynchronous operations that can run in parallel
+                  Overall Time => max( P1,P2,P3....) 
+                  
+    Nesting Promises: chaining.then(), execute asynchronous operations sequentially.
+                  Each .then() block waits for the previous promise to resolve before executing it.
+                  Suitable when a chain of dependent asynchronous operations need to be executed sequentially.
+                  Overall Time => ( P1+P2+P3+....)
+*/
 function waitOneSecond() {
-
-}
-
-function waitTwoSecond() {
-
-}
-
-function waitThreeSecond() {
-
-}
-
-function calculateTime() {
-
-}
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log("Promise 1 Resolved");
+        resolve();
+      }, 1000);
+    });
+  }
+  
+  function waitTwoSecond() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log("Promise 2 Resolved");
+        resolve();
+      }, 2000);
+    });
+  }
+  
+  function waitThreeSecond() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log("Promise 3 Resolved");
+        resolve();
+      }, 3000);
+    });
+  }
+  
+  function calculateTime() {
+    const startTime = Date.now();
+  
+    return waitOneSecond()
+      .then(() => waitTwoSecond())
+      .then(() => waitThreeSecond())
+      .then(() => {
+        const endTime = Date.now();
+        const elapsedTime = (endTime - startTime) / 1000; // Convert to seconds
+        console.log(`Time taken: ${elapsedTime} seconds`);
+      });
+  }
+  
+  calculateTime();
+  
