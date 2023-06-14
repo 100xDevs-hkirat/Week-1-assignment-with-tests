3859226e80c2f6e9c5fde11ab0bf09fe95b340a4
Ayush Tiwari <97670807+ayushtiwari77@users.noreply.github.com>
2023-06-14 14:04:10 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -1,6 +1,7 @@
 /*
   Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
+ 
+   object is unique category-wise and has total price spent as its value.
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 
@@ -8,8 +9,50 @@
   - `npm run test-expenditure-analysis`
 */
 
+/* let categoryMap = {};
+
+  // Calculate total spent for each category
+  for (let transaction of transactions) {
+    let category = transaction.category;
+    let price = transaction.price;
+
+    // If the category is already present in the categoryMap, add the price to the existing total
+    if (categoryMap.hasOwnProperty(category)) {
+      categoryMap[category] += price;
+    } else {
+      // If the category is not present, initialize it with the price
+      categoryMap[category] = price;
+    }
+  }
+
+  // Convert the categoryMap to an array of objects in the desired format
+  let result = [];
+  for (let category in categoryMap) {
+    result.push({ category: category, totalSpent: categoryMap[category] });
+  }
+
+  return result;
+}
+*/
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let empty = {};
+  for (const i of transactions) {
+    let category = i.category;
+    let price = i.price;
+
+    if (empty.hasOwnProperty(category)) {
+      empty[category] += price;
+    } else {
+      empty[category] = price;
+    }
+  }
+
+  let result = [];
+  for (const category in empty) {
+    result.push({ category: category, totalSpent: empty[category] });
+  }
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
