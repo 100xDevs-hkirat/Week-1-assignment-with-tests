6406db9c2be874010dede8c5c18133f539cb7472
AtwalGagan <gagan.atwal12@gmail.com>
2023-06-15 00:44:01 +0530

    Easy and one medium assignmnet added for now

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,40 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
-
+    var finalOutput=[] 
+    if (transactions.length == 0){
+      return finalOutput
+    }else{
+        for (var i =0 ; i<transactions.length; i++){
+          var matched = false
+          obj = {}
+          if (finalOutput.length ==0){
+            obj['category'] =  transactions[i].category
+            obj['totalSpent'] = transactions[i].price
+            finalOutput.push(obj)
+            continue
+          }else{
+            console.log("Inside else")
+            for (var j=0; j<finalOutput.length; j++){
+              if (finalOutput[j].category==transactions[i].category){
+                finalOutput[j].totalSpent = finalOutput[j].totalSpent + transactions[i].price
+                console.log("Inside else-if line 41",finalOutput[j].totalSpent)
+                matched = true
+                continue
+              }
+            } 
+            console.log(finalOutput)
+            if (matched == false){
+              obj['category'] =  transactions[i].category
+              obj['totalSpent'] = transactions[i].price
+              finalOutput.push(obj)
+            }
+          }
+        }
+    
+        return finalOutput;
+      }
+      
+    }
+  
 module.exports = calculateTotalSpentByCategory;
