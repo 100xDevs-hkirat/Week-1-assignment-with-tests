ca0eada4703c198ceddd15aae4bf045aab597b70
Ankith Lagupudi <Lagupudiankith@gmail.com>
2023-06-14 23:46:10 +0530

    Full Stack Development Course Assignment 1 Part 1 -- Added Code for anagram.js, calculator.js, expenditure-analysis.js, palindrome.js, times.js and todo-list.js and ran tests

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -17,6 +17,49 @@
   - `npm run test-calculator`
 */
 
-class Calculator {}
+class Calculator {
+    constructor() {
+        this.result = 0;
+    }
+
+    add(number){
+        this.result += number;
+    }
+
+    subtract(number){
+        this.result -= number;
+    }
+
+    multiply(number){
+        this.result *= number;
+    }
+
+    divide(number){
+        if (number === 0) {
+            throw new Error("Division by zero is not allowed.");
+        }
+        this.result /= number;
+    }
+
+    clear(){
+        this.result = 0;
+    }
+
+    getResult(){
+        return this.result;
+    }
+
+    calculate(expression) {
+        const sanitizedExpression = expression.replace(/\s+/g, "");
+
+        const result = eval(sanitizedExpression);
+
+        if (isNaN(result) || !isFinite(result)) {
+            throw new Error("Invalid expression.");
+        }
+
+        this.result = result;
+    }
+}
 
 module.exports = Calculator;
