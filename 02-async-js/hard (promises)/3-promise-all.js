41cc06231688189be7e583a8249fea047a27c31f
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-16 00:14:41 +0530

    promise all resolved

--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -4,19 +4,43 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
+const { error } = require("console");
+const { resolve } = require("path");
 
 function waitOneSecond() {
-
+  return new Promise((resolve) => {
+    // console.log("Promise 1 started");
+    setTimeout(() => {
+      resolve("Promise 1 started");
+    }, 1000);
+  });
 }
 
 function waitTwoSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Promise 2 done");
+    }, 2000);
+  });
 }
 
 function waitThreeSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("wait three done");
+    }, 3000);
+  });
 }
 
 function calculateTime() {
-
+  const startTime = new Date();
+  return Promise.all([waitOneSecond, waitTwoSecond, waitThreeSecond])
+    .then((result) => {
+      const endTime = new Date();
+      console.log(`the took for calculation ${endTime - startTime}`);
+      console.logo("Resolved", result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
