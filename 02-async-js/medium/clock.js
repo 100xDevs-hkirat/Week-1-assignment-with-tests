f8f85d4d700707c69ccf4a75923fd087b2cf90a5
Siddharth Patel <105500935+siddharth0998@users.noreply.github.com>
2023-06-15 14:42:59 +0530

    medium-clock-task-done

---
+++ 02-async-js/medium/clock.js
@@ -0,0 +1,19 @@
+function updateTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours() < 10 ? 0 + `${currentTime.getHours()}` : currentTime.getHours();
+    const minutes = currentTime.getMinutes() < 10 ? 0 + `${currentTime.getMinutes()}` : currentTime.getMinutes();
+    const seconds = currentTime.getSeconds() < 10 ? 0 + `${currentTime.getSeconds()}` : currentTime.getSeconds();
+
+    const formattedTime24 = `${hours}:${minutes}:${seconds}`;
+    console.log(formattedTime24);
+
+    const hours12 = hours > 11 ? ((hours - 12) < 10 ? 0 + `${hours - 12}` : hours - 12) : hours
+    const msg = hours > 11 ? 'PM' : 'AM'
+
+    const formattedTime12 = `${hours12}:${minutes}:${seconds} ` + msg
+    console.log(formattedTime12)
+}
+
+setInterval(updateTime, 1000);
+
+
