838b2b486e2ba256d906a74e3f0972b11951e026
Karan Arora <nsit_karan@yahoo.com>
2023-06-15 22:26:29 +0530

    show time and refresh every second

---
+++ 02-async-js/medium/2-clock.js
@@ -0,0 +1,17 @@
+/**
+ * Using `1-counter.md` or `2-counter.md` from the easy section, can you create a
+ * clock that shows you the current machine time?
+ * 
+ * Can you make it so that it updates every second, and shows time in the following formats - 
+ * HH:MM::SS (Eg. 13:45:23)
+ * HH:MM::SS AM/PM (Eg 01:45:23 PM)
+ */
+function timeCallback() {
+    let d = new Date();
+    console.clear();
+    console.log(d.toLocaleTimeString())
+    console.log(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds());
+
+}
+
+setInterval(timeCallback, 1000);
