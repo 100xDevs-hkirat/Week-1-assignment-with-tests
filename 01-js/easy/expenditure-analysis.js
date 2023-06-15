65aa15eddb48c11662245c3df770ccc707d626e8
Aayushman Jain <94888896+AayushTech@users.noreply.github.com>
2023-06-15 23:45:04 +0530

    Update expenditure-analysis.js
    adding the Solution for Expenditure-analysis the solution is not optimized will pr the optimized ones I get time

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,38 @@
   - `npm run test-expenditure-analysis`
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+ //function to return list of unique category from transaction
+function uniqlist(transaction){
+let ul = []
+for (let i = 0; i < transaction.length; i++) {
+  if (!ul.includes(transaction[i].category)) {
+    ul.push(transaction[i].category);
+  }
+}
+return ul
+}
+
+// function to calculate TotalSpent By Category 
+
+function calculateTotalSpentByCategory(transaction){
+  uniqlist = uniqlist(t)
+let list = []
+for(let j=0;j<uniqlist.length;j++){
+  
+let obj = {}
+  
+let count = 0
+  
+for(let k=0;k<t.length;k++){
+  if (t[k].category === uniqlist[j]){
+     count = count + t[k].price
+  }
+} 
+   obj['category'] = uniqlist[j]
+   obj['totalSpent'] = count
+  list.push(obj)
+ }
+  return list
 }
 
 module.exports = calculateTotalSpentByCategory;
