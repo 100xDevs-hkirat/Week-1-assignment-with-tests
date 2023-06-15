9c2f88565326a1e6183e9473c4080b389b4461a7
Muzaffar Hossain <muzaffar640@gmail.com>
2023-06-15 23:49:47 +0530

    sleep completely done

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,13 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+  const now = Date.now();
+  console.log("starting");
+  while (Date.now() - now < seconds * 1000) {
+    console.log("waiting");
+  }
+  console.log("done");
 }
+
+sleep(3);
