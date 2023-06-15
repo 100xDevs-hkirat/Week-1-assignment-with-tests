388a42ce0ca43e97cc7f929f81bc31832d561fca
Rajat <rajat.bhardwaj@dpworld.com>
2023-06-15 21:49:08 +0530

    Solved Easy anagram problem

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,16 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length !== str2.length) {
+    return false;
+  }
+  
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  str1 = str1.split('').sort().join('');
+  str2 = str2.split('').sort().join('');
 
+  return str1 == str2;
 }
 
 module.exports = isAnagram;
