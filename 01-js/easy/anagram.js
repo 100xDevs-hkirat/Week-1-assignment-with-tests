e16d7c5c4ff45643f6fc0e1aa7ae806aef7d58d4
mohammed <mansoormed@gmail.com>
2023-06-15 22:48:30 +0530

    Working on anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,49 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
-
+function createDict(str1){
+  dict1 = new Map();
+  for(i of str1){
+    if(dict1.has(i)){
+      dict1.set(i,dict1.get(i) + 1);
+    }
+    else{
+      dict1.set(i,1);
+    }
+  }
+  console.log(dict1);
+  console.log(dict1.size)
+  return dict1;
 }
+
+function isEquivalent(dict1, dict2){
+  if (dict1.size !== dict2.size){
+    return false;
+  }
+  for (x of dict1.keys()){
+    if(!dict2.has(x)){
+      return false;
+    }
+    else{
+      if(dict1.get(x) !== dict2.get(x)){
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+
+function isAnagram(str1, str2) {
+  var a = str1.toLowerCase();
+  var b = str2.toLowerCase();
+  let x = createDict(a);
+  let y = createDict(b);
+  var v = isEquivalent(x,y);
+  return v;
+}
+
+
 
 module.exports = isAnagram;
