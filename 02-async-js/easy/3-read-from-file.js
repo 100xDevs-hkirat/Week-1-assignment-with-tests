73bf73f723d3949ce1cb8c4cc6779a830ba69bf7
Siddharth Patel <105500935+siddharth0998@users.noreply.github.com>
2023-06-15 13:07:23 +0530

    3-read-from-file-task-done

---
+++ 02-async-js/easy/3-read-from-file.js
@@ -0,0 +1,17 @@
+const fs = require('fs'),
+      path = require('path'),
+      filePath = path.join(__dirname, 'text.txt')
+   
+fs.readFile(filePath, 'utf-8', (err,data) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+    console.log(data)
+})
+
+let sum=0
+for(let i=0; i<= 10000000; i++){
+   sum += i
+}
+console.log(sum)
