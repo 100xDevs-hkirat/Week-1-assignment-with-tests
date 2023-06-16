3fe98f03ff8dc50338be4414878db9837d1a1331
Ashok <akkumar0069@gmail.com>
2023-06-16 12:38:08 +0530

    Solved 01-js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,26 @@
 */
 
 function isAnagram(str1, str2) {
+    if (str1 == null || str2 == null || str1.length != str2.length) {
+        return false;
+    }
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    let alphabetMapOfStr1 = createMap(str1);
+    for (let ch of str2) {
+        if (!alphabetMapOfStr1[ch]) {
+            return false;
+        }
+    }
+    return true;
+}
 
+function createMap(str) {
+    let alphabetMap = {};
+    for (let ch of str) {
+        alphabetMap[ch] = 1;
+    }
+    return alphabetMap;
 }
 
 module.exports = isAnagram;
--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -1,18 +1,42 @@
 /*
   Implement a class `Todo` having below methods
-    - add(todo): adds todo to list of todos
-    - remove(indexOfTodo): remove todo from list of todos
-    - update(index, updatedTodo): update todo at given index
-    - getAll: returns all todos
-    - get(indexOfTodo): returns todo at given index
-    - clear: deletes all todos
+	- add(todo): adds todo to list of todos
+	- remove(indexOfTodo): remove todo from list of todos
+	- update(index, updatedTodo): update todo at given index
+	- getAll: returns all todos
+	- get(indexOfTodo): returns todo at given index
+	- clear: deletes all todos
 
   Once you've implemented the logic, test your code by running
   - `npm run test-todo-list`
 */
 
 class Todo {
-
+	constructor() {
+		this.todos = [];
+	}
+	add(todo) {
+		this.todos.push(todo)
+	}
+	remove(index) {
+		if(index<this.todos.length)
+			this.todos.splice(index, 1);
+	}
+	update(index, updatedTodo) {
+		if(index<this.todos.length)
+			this.todos[index] = updatedTodo;
+	}
+	getAll() {
+		return this.todos;
+	}
+	get(index) {
+		if(index<this.todos.length)
+			return this.todos[index];
+		return null;
+	}
+	clear() {
+		this.todos = [];
+	}
 }
 
 module.exports = Todo;
