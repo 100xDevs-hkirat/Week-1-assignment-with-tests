5465a289b6e1a9e1bea2f1c1780c40b383f43689
Viraj <virajjadhao85@gmail.com>
2023-06-16 22:21:34 +0530

    all done

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -4,5 +4,14 @@
  */
 
 function sleep (seconds) {
-
+    return new Promise((res)=>{
+        setTimeout(res,seconds)
+    })
 }
+
+async function temp(){
+    console.log("I got the thread first")
+    await sleep(2000)
+    console.log("why i got thread after two seconds :(")
+}
+temp()
