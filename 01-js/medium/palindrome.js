13c343e65ee252c3763d8fcf559d13fc24977432
Souvick Mukherjee <souvick.mukherjee871@gmail.com>
2023-06-17 00:57:51 +0530

    palindrome completed

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,27 @@
 */
 
 function isPalindrome(str) {
+  var l=str.length;
+  f=1;
+  var regex= /[,.?! ]/g;
+  
+  var revStr=str.split("").reverse().join("");
+  var st1=str.toLowerCase().replace(regex,"");
+  var st2=revStr.toLowerCase().replace(regex,"");
+  for(var i=0;i<l;i++)
+  {
+    if(st1[i]!=st2[i] )
+    {
+      f=0;
+    }
+    
+  }
+  if(f===1)
+  {
   return true;
+  }
+  else
+  return false
 }
 
 module.exports = isPalindrome;
