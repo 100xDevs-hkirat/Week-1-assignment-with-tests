8f65289662f067aec309c009b5144d57537f54f9
Hemanth <mallahemanthnaidu2001@gmail.com>
2023-06-16 20:56:23 +0530

    expenditure-analysis with tests

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,32 @@
   - `npm run test-expenditure-analysis`
 */
 
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryTotals = {};
+
+  for (const transaction of transactions) {
+    const { category, price } = transaction;
+
+    if (category in categoryTotals) {
+      categoryTotals[category] += price;
+    } else {
+      categoryTotals[category] = price;
+    }
+  }
+
+  const result = Object.entries(categoryTotals).map(([category, totalSpent]) => ({
+    category,
+    totalSpent,
+  }));
+
+  return result;
 }
+
+
+
+
+
+var sol = calculateTotalSpentByCategory([])
 
 module.exports = calculateTotalSpentByCategory;
