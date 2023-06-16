9b3194fc2753486a04aeaf8adf89358bf3b0aff1
Dibyanshu <dibyansh01iitk@gmail.com>
2023-06-16 19:33:01 +0530

    completed easy part of the assignment1-02

--- 02-async-js/easy/4-write-to-file.js
+++ 02-async-js/easy/4-write-to-file.js
@@ -1,3 +1,16 @@
-## Write to a file
-Using the fs library again, try to write to the contents of a file.
-You can use the fs library to as a black box, the goal is to understand async tasks.
+// ## Write to a file
+// Using the fs library again, try to write to the contents of a file.
+// You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require('fs');
+function callback(err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("content has been successfully written");
+    }
+  }
+
+const content = 'Hi! This is my first written file using javsscript.';
+
+fs.appendFile('Week-1-assignment-with-tests/02-async-js/easy/file.txt', content, callback)
