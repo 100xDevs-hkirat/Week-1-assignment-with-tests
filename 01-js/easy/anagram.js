10735f7afd807114ffb7e9c7b44ac0d2c81b64a0
joyal007 <joyalraphel00@gmail.com>
2023-06-17 21:31:03 +0530

    done easy anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,42 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.toLowerCase()
+  str2 = str2.toLowerCase()
+  let alphaStr1 = {}
+  for(let i = 0; i< str1.length; i++){
+    if( str1[i] in alphaStr1){
+      alphaStr1[str1[i]] +=1
+    }else{
+      alphaStr1[str1[i]]=1
+    }
+  }
+  // console.log(alphaStr1)
+  console.log(str1,str2)
+  for(let j = 0;j< str2.length;j++){
+    if(str2[j] in alphaStr1){
+      if(alphaStr1[str1[j]]==0){
+        // console.log("0")
+        return false
+      }
+      alphaStr1[str1[j]] -=1
+    }else{
+      // console.log("empty")
+      return false
+    }
+  }
+  let ret = true
+  Object.keys(alphaStr1).forEach((e)=>{
+    if(alphaStr1[e]!=0){
+      console.log(alphaStr1[e])
+      ret = false
+    }
+  })
+  return ret
 }
+
+const ret = isAnagram("openai","open")
+console.log(ret)
+
 
 module.exports = isAnagram;
