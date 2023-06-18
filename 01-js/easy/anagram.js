8e787555804809f72b855536795db3bd7d0b2178
Lokeshwar Reddy Jonnalagadda <10lokeshnani@gmail.com>
2023-06-18 19:29:39 +0530

    Solved 01-js Easy Problems

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -9,6 +9,56 @@
 
 function isAnagram(str1, str2) {
 
+    if (str1.length != str2.length) {
+        return false;
+    }
+
+    let countObject = {};
+
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    
+    for (let i=0; i<str1.length; ++i) {
+        let char = str1.charAt(i);
+        if (countObject.hasOwnProperty(char)) {
+            let count = countObject[char];
+            ++count;
+            countObject[char] = count;
+        } else {
+            countObject[char] = 1;
+        }
+    }
+
+    // console.log("Object After STR1: ", countObject);
+
+    for (let i=0; i<str2.length; ++i) {
+        let char = str2.charAt(i);
+        if (countObject.hasOwnProperty(char)) {
+            let count = countObject[char];
+            --count;
+            countObject[char] = count;
+        } else {
+            return false;
+        }
+    }
+
+    // console.log("Object After STR2: ", countArray);
+
+    let allValues = Object.values(countObject);
+
+    for (let i=0; i<allValues.length; ++i) {
+        if (allValues[i] != 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
+
+// let flag = isAnagram("Hello", "World");
+// console.log("FLAG: ", flag);
+
+// flag = isAnagram("silent", "listen");
+// console.log("FLAG: ", flag);
 
 module.exports = isAnagram;
