acab1fb17051975d11e8dd334a758ae97947bce7
Ashutosh Vimal <ashutoshvimal22@gmail.com>
2023-06-20 01:53:01 +0530

    🤯 sleep ye async fn k andar agar promis return ho raha to roak deta h

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,14 +3,23 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-    let start = Date.now();
+// function sleep (seconds) {
+//     let start = Date.now();
 
-    while(Date.now() - start < seconds*1000){}
+//     while(Date.now() - start < seconds*1000){}
+// }
+let cnt = 0;
+async function delay(seconds){
+    return new Promise( (resolve ) => setTimeout(resolve, seconds*1000))
+}
+async function sleep(seconds){
+    let res = await delay(seconds);
+    console.log(`after ${seconds} seconds`);
+    return res;
 }
 
+sleep(3);
 
-let n = 5
-sleep(n);
-
-console.log(`was printed after ${n} seconds`)
+for(let i=0;i<10;i++){
+    console.log(i);
+}
