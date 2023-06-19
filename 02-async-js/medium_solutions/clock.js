0f3155911d0f01f18b04aafcf326f24a3727e0eb
Divakar <divaakaar.saakthi@gmail.com>
2023-06-19 14:37:59 +0530

    Added solution to clock

---
+++ 02-async-js/medium_solutions/clock.js
@@ -0,0 +1,22 @@
+/* 
+
+Can you make it so that it updates every second, and shows time in the following formats - 
+
+ - HH:MM::SS (Eg. 13:45:23)
+
+ - HH:MM::SS AM/PM (Eg 01:45:23 PM)
+*/
+
+function clock() { 
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours%=12;
+    let time = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
+    console.log(time);
+}
+
+setInterval(clock, 1000);
+
