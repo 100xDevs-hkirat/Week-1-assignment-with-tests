5863a29d791ee879e1106e7c72026a6843cbd3e4
NAVEEN ENGINEER <69977405+nsundriyal62@users.noreply.github.com>
2023-06-19 21:19:14 +0530

    pta nhi kya kya kr rha hu
    sorry agar khuch galat hua to 
    mujhe fetch aur fork aur commit ka scene thoda palle nhi pdra
    to jha jo banra wo kr ra
    
    :)

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,25 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1=str1.toLowerCase();
+  str2=str2.toLowerCase();
+  str1=str1.split('');
+  str2=str2.split('');
+  str2=str2.sort();
+  str1=str1.sort();
+  str2=str2.join('');
+  str1=str1.join('');
+if(str1==str2){
+  return true;
+}else{
+  return false;
+}
 }
 
+
+// if('naveen '==['naveen']){
+//     console.log('nave');
+// }else{
+//     console.log('not ');
+// }
 module.exports = isAnagram;
