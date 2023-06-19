1cf5a00466008848d4045b5571a553526199edf6
Divakar <divaakaar.saakthi@gmail.com>
2023-06-19 14:34:06 +0530

    added partial solutions for easy and medium questions

---
+++ 02-async-js/medium_solutions/file_cleaner.js
@@ -0,0 +1,35 @@
+/*
+
+Read a file, remove all the extra spaces and write it back to the same file.
+
+For example, if the file input was
+```
+hello     world    my    name   is       raman
+```
+
+After the program runs, the output should be
+
+```
+hello world my name is raman
+```
+
+*/
+
+var fs = require('fs'); 
+
+function fileCleaner(filePath) {
+    fs.readFile(filePath, 'utf8', function(err, data) {
+        if (err) {
+        console.log('Error: ', err);
+        } else {
+        var newStr = data.replace(/\s+/g, ' ');
+        fs.writeFile(filePath, newStr, function(err) {
+            if (err) {
+            console.log('Error: ', err);
+            }
+        });
+        }
+    });
+}
+
+fileCleaner('./02-async-js/medium_solutions/text.txt');
