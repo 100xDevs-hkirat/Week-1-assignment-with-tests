f4f1b5b3636a81a7152d96d243d78ce6cb220793
prasanna <bhursuprasannakumar@gmail.com>
2023-06-19 10:02:20 +0530

    completed all async tasks

--- 02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,4 +3,20 @@
 */
 
 function wait(n) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      resolve("Completed");
+    }, n * 1000);
+  });
 }
+
+const n = 7;
+const waited = wait(n);
+
+console.log(waited);
+
+waited.then(function (status) {
+  console.log(waited);
+  console.log(status);
+  console.log(`promise completed in ${n} seconds`);
+});
