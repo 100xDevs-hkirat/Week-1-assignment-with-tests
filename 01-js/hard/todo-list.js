8f5d88c330ee20932496b99706a45a6a7e2d9d51
Rasendra Kaushik <117708193+RasendraKaushik@users.noreply.github.com>
2023-06-20 07:36:17 +0530

    Update todo-list.js
    Yo

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,50 @@
 */
 
 class Todo {
+  constructor(){
+    this.list = [];
+  }
+  #validIndex(idx) {
+    return idx < this.list.length && idx >= 0;
+  }
+  add(todo) {
+    this.list.push(todo);
+  }
 
+  remove(idx) {
+    this.list.splice(idx, 1);
+  }
+
+  update(idx, newTodo) {
+    this.#validIndex(idx) && this.list.splice(idx, 1, newTodo);
+  }
+
+  getAll() {
+    return this.list;
+  }
+
+  get(idx) {
+    return this.#validIndex(idx) ? this.list[idx] : null;
+  }
+
+  clear() {
+    this.list = [];
+  }
 }
 
+const todoList = new Todo();
+todoList.add('Buy groceries');
+todoList.add('Finish homework');
+todoList.add('Call friend');
+
+console.log(todoList.getAll());  // Output: ['Buy groceries', 'Finish homework', 'Call friend']
+
+todoList.update(1, 'Complete assignment');
+console.log(todoList.get(1));  // Output: 'Complete assignment'
+
+todoList.remove(2);
+console.log(todoList.getAll());  // Output: ['Buy groceries', 'Complete assignment']
+
+todoList.clear();
+console.log(todoList.getAll()); 
 module.exports = Todo;
