77748a96eb16d99eb65d133b90f299262fccad20
anirudh.ponna <anirudh.ponna@appviewx.com>
2023-06-21 21:54:03 +0530

    Implemented AM/PM in clock

--- 02-async-js/medium/2-clock.js
+++ 02-async-js/medium/2-clock.js
@@ -4,14 +4,40 @@ let now = new Date();
 let seconds = now.getSeconds();
 let minutes = now.getMinutes();
 let hours = now.getHours();
+let hours12 = hours;
+let isAM = true;
 let paddedMinutes, paddedSeconds, paddedHours;
 
-function printTime() {
-    console.clear();
+function printTime(AMPM=false, isAM=true) {
     paddedHours = hours.toString().padStart(2,"0");
+    paddedHours12 = hours12.toString().padStart(2,"0");
     paddedMinutes = minutes.toString().padStart(2,"0");
     paddedSeconds = seconds.toString().padStart(2, "0");
-    console.log(paddedHours,":",paddedMinutes,":",paddedSeconds);
+    if(AMPM){
+        if(isAM){
+            console.log(paddedHours12,":",paddedMinutes,":",paddedSeconds," AM");
+        }
+        else {
+            console.log(paddedHours12,":",paddedMinutes,":",paddedSeconds," PM");
+        }
+    }
+    else {
+        console.log(paddedHours,":",paddedMinutes,":",paddedSeconds);
+    }
+}
+
+function printTime12hrFormat(){
+    if(hours==0){
+        hours12 = 12;
+    }
+    if(hours>12){
+        hours12 = hours-12;
+        isAM = false;
+    }
+    else{
+        isAM = true;
+    }
+    printTime(AMPM=true, isAM);
 }
 
 function incrementSeconds(){
@@ -20,7 +46,9 @@ function incrementSeconds(){
         seconds=0;
         incrementMinutes();
     }
+    console.clear();
     printTime();
+    printTime12hrFormat();
 }
 
 function incrementMinutes(){
@@ -30,12 +58,14 @@ function incrementMinutes(){
         incrementHours();
     }
     printTime();
+    printTime12hrFormat();
 }
 
 function incrementHours(){
     hours++;
     if(hours>23) hours = 0;
     printTime();
+    printTime12hrFormat();
 }
 
 
