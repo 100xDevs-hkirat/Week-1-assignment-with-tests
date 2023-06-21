f49ecd64742ad7e3444a0a039e55675840007e30
Karan kumar <86042278+Karankumar2212@users.noreply.github.com>
2023-06-21 20:29:05 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,6 +8,11 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.toLowerCase().split("").sort().join("") == str2.toLowerCase().split("").sort().join("")){
+return true;
+  }else{
+return false;
+  }
 
 }
 
