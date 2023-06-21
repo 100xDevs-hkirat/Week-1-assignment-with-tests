180171516aa01f3955d806e8414cbd09c7628945
anirudh.ponna <anirudh.ponna@appviewx.com>
2023-06-21 21:35:53 +0530

    Solved Hard Problems

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -2,7 +2,14 @@
  * Write a function that halts the JS thread (make it busy wait) for a given number of milliseconds.
  * During this time the thread should not be able to do anything else.
  */
-
 function sleep (seconds) {
-
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000))
 }
+
+console.log("This will execute");
+async function haltJS(){
+    await sleep(3);
+    console.log('This will execute after 3 seconds because Javascript is waiting for the above to finish (await keyword)');
+}
+haltJS();
+console.log("This will also execute");
--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -6,17 +6,51 @@
 
 
 function waitOneSecond() {
-
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), 1000);
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), 2000);
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), 3000);
+    })
 }
 
 function calculateTime() {
-
+    const startTime = new Date().getTime();
+    waitOneSecond().then(() => {
+        console.log("First completed");
+    });
+    waitTwoSecond().then(() => {
+        console.log("Second completed");
+    });
+    waitThreeSecond().then(() => {
+        console.log("Third completed");
+    });
+    const endTime = new Date().getTime(); 
+    // This won't give the correct time since all the above are async functions and this is a synchronous step.
+    return (endTime - startTime) / 1000;
 }
+
+console.log(calculateTime());
+
+// Instead wait until all promises are done and then calculate end time.
+function calculateRealTime(){
+    let startTime = new Date().getTime();
+    Promise.all([waitOneSecond(), waitTwoSecond(), waitThreeSecond()]).then(() => {
+        let endTime = new Date().getTime();
+        let timeTaken = (endTime - startTime) / 1000;
+        console.log("Time for all promises to finish")
+        console.log(timeTaken);
+    })
+}
+
+// The real time is close to 3 seconds as 3 is the maximum time of all promises.
+calculateRealTime();
