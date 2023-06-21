e882a4547a3c68da7b0ff97494d9a6b0e2040966
Karan <karankumar20001222@gmail.com>
2023-06-21 22:56:05 +0530

    Coded the expenditure analysis problem

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,54 @@
   - `npm run test-expenditure-analysis`
 */
 
+
+var transactions = [
+  {
+    itemName: "Maggi",
+    category: "Food", 
+    price: 20,
+    timestamp: 22/2/2023
+  },
+  {
+    itemName: "Limca",
+    category: "Drink", 
+    price: 200,
+    timestamp: 22/2/2023
+  },
+  {
+    itemName: "Oreo",
+    category: "Food", 
+    price: 60,
+    timestamp: 22/2/2023
+  }
+]
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let totalSpent = {};
+
+  for(let i=0;i<transactions.length;i++){
+    let t = transactions[i];
+    if(totalSpent[t.category]){
+      totalSpent[t.category] = totalSpent[t.category] + t.price ;
+    }else{
+      totalSpent[t.category] = t.price;
+    }
+  }
+
+  let keys = Object.keys(totalSpent);
+
+  let answer=[];
+
+  for(let i=0;i<keys.length;i++){
+    var category = keys[i];
+    let obj={
+      category: category,
+      totalSpent: totalSpent[category]
+    }
+    answer.push(obj);
+  }
+
+  return answer;
 }
 
 module.exports = calculateTotalSpentByCategory;
