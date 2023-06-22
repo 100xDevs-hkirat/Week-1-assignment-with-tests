b87882f2f9186ae4540b9ee3cd2a83087f71966d
Ayush <abhipatel9607@gmail.com>
2023-06-22 12:30:51 +0530

    Solution by Abhishek- update 01

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,66 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let arr = [];
+  let obj = {};
+  for (const tran of transactions) {
+    if (obj[tran["category"]]) {
+      obj[tran["category"]] += tran["price"];
+    } else {
+      obj[tran["category"]] = tran["price"];
+    }
+  }
+  for (const [key, value] of Object.entries(obj)) {
+    arr.push({ category: key, totalSpent: value });
+  }
+  return arr;
 }
+
+let transactions = [
+  {
+    id: 1,
+    itemName: "SmartPhone",
+    category: "Technology",
+    price: 30000,
+    timestamp: "7686976968",
+  },
+  {
+    id: 2,
+    itemName: "T-Shirt",
+    category: "Clothes",
+    price: 1099,
+    timestamp: "5795678757",
+  },
+  {
+    id: 3,
+    itemName: "Himalaya-FaceWash",
+    category: "Skin-Care",
+    price: 399,
+    timestamp: "7876897668",
+  },
+  {
+    id: 4,
+    itemName: "Cooker-5L",
+    category: "Kitchen",
+    price: 3999,
+    timestamp: "7678968680",
+  },
+  {
+    id: 5,
+    itemName: "EarPhone-Bluetooth",
+    category: "Sound",
+    price: 2599,
+    timestamp: "7687969689",
+  },
+  {
+    id: 3,
+    itemName: "Himalaya-FaceWash",
+    category: "Skin-Care",
+    price: 9000,
+    timestamp: "7876897668",
+  },
+];
+const totalSpentByCategory = calculateTotalSpentByCategory(transactions);
+console.log(totalSpentByCategory);
 
 module.exports = calculateTotalSpentByCategory;
