f4d8f5d18b80d988fbef4d669bfd3e164d5f7140
manosatpathy <manosatpathy@gmail.com>
2023-06-23 20:15:22 +0530

    completing expenditure-analysis

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -8,8 +8,58 @@
   - `npm run test-expenditure-analysis`
 */
 
+let allTransactions = [
+  {
+    itemName: "camera",
+    category: "electronics",
+    price: 90000,
+    timestamp: 1234334,
+  },
+  {
+    itemName: "smart tv",
+    category: "appliances",
+    price: 16000,
+    timestamp: 1234334,
+  },
+  {
+    itemName: "laptop",
+    category: "electronics",
+    price: 62000,
+    timestamp: 12335634,
+  },
+  {
+    itemName: "oneplus",
+    category: "mobiles",
+    price: 52000,
+    timestamp: 1237334,
+  },
+  {
+    itemName: "washing machine",
+    category: "appliances",
+    price: 16000,
+    timestamp: 1294334,
+  },
+];
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let obj = {};
+
+  for (let i = 0; i < transactions.length; i++) {
+    if (obj[transactions[i].category]) {
+      obj[transactions[i].category] += transactions[i].price;
+    } else {
+      obj[transactions[i].category] = transactions[i].price;
+    }
+  }
+  let keys = Object.keys(obj);
+  let answer = [];
+  for (let i = 0; i < keys.length; i++) {
+    let category = keys[i];
+    answer.push({ category: category, totalSpent: obj[category] });
+  }
+  return answer;
 }
 
+let newArray = calculateTotalSpentByCategory(allTransactions);
+console.log(newArray);
 module.exports = calculateTotalSpentByCategory;
