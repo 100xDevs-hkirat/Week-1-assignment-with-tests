db0a2053f6e960322fe8a0967da071849c8c98e2
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-24 01:24:09 +0530

    i think it is right as off now now

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,8 +6,16 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-anagram`
 */
-
 function isAnagram(str1, str2) {
+  // convert it into array then apply sorting
+  var arrayStr1 = str1.split('').sort();
+  var arrayStr2 = str2.split('').sort();
+
+  var sortStr1 = str1.join('');
+  var sortStr2 = str2.join('');
+
+  return sortStr1 == sortStr2;
+
 
 }
 
