fea5a4ca097c8aaa8c1b8dc80e83bed0d5575c01
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2023-06-24 23:26:43 +0530

    Palindrome completed

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,30 @@
 */
 
 function isPalindrome(str) {
+  function removeSymbols(string) {
+    const acceptableChars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    let newString = "";
+    for (const char of string) {
+      if (acceptableChars.includes(char)) {
+        newString += char;
+      }
+    }
+    return newString;
+  }
+  str = str.toLowerCase().replaceAll(" ", "");
+
+  str = removeSymbols(str);
+
+  for (let i = 0; i < str.length; i++) {
+    const start = i;
+    const end = str.length - i - 1;
+
+    if (str[start] !== str[end]) return false;
+
+    if (start >= end) break;
+  }
   return true;
 }
+
 
 module.exports = isPalindrome;
