a7197da2c5d28d3c7c2d0876a5494bf91339bee4
Sreehari v p <svp22080@gmail.com>
2023-06-26 19:11:31 +0530

    completed the whole assignment

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,14 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
+function sleep(seconds) {
+    const date = new Date();
+    let time = date.getTime();
+    let time2 = time;
+    do {
+        const date2 = new Date();
+        time2 = date2.getTime();
 
+    } while ((time2 - time) <= seconds * 1000);
 }
+
