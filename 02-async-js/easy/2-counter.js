7a195f2a9651fe1ecd1dec09f028ec9cb5f2c231
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-06-27 03:55:53 +0530

    solved by recurively

--- 02-async-js/easy/2-counter.js
+++ 02-async-js/easy/2-counter.js
@@ -1,9 +1,18 @@
-## Counter without setInterval
+// ## Counter without setInterval
 
-<!-- Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck. -->
+// <!-- Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck. -->
 
 
+var counter = 1;
 
+function printAndIncreaseCount() {
+    console.clear();
+    console.log(counter)
+    counter += 1;
+    setTimeout(printAndIncreaseCount, 1000);
+}
+
+setTimeout(printAndIncreaseCount, 1*1000);
 
 
 
