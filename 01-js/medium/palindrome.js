afa2ac81143606d572350d325ccd04ef0b7261c9
Aftab Naik <aftabnaik1419@gmail.com>
2023-07-02 04:30:48 +0530

    Solved palindrome question from week 1 medium

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,9 +5,68 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
-
-function isPalindrome(str) {
-  return true;
+let lowercase_alphabets = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+function is_alphabet(x) {
+  for (let i = 0; i < lowercase_alphabets.length; i++) {
+    if (x == lowercase_alphabets[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
 
+function without_punctuation_spaces(str) {
+  str=str.toLowerCase();
+  var ans = "";
+  for (let i = 0; i < str.length; i++) {
+    if (is_alphabet(str[i])) {
+      ans += str[i];
+    }
+  }
+  return ans;
+}
+function isPalindrome(str) {
+  str=str.toLowerCase();
+  let str1 = without_punctuation_spaces(str);
+  // let str2 = without_punctuation_spaces(str);
+  // let str1 = str.toLowerCase();
+  let str2 = reverseString(str1);
+  if (str1 == str2) return true;
+  else return false;
+}
+
+ 
 module.exports = isPalindrome;
+ 
+ 
+ 
