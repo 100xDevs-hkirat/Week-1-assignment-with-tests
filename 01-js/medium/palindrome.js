88c0e02d63b7ed7703eee56939c5f7871ddccdcc
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-07-04 10:13:53 +0530

    palindrome completed with all test cases

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,8 +5,33 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
+function isPunctuation(char) {
+  const punctuationRegex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
+  return punctuationRegex.test(char);
+}
 
 function isPalindrome(str) {
+  var newStr = str.toLowerCase();
+  console.log(newStr);
+  var start = 0,
+    end = newStr.length - 1;
+
+  while (start <= end) {
+    if (newStr[start] === " " || isPunctuation(newStr[start])) {
+      console.log("1");
+      start++;
+    } else if (newStr[end] === " " || isPunctuation(newStr[end])) {
+      console.log("2");
+      end--;
+    } else if (newStr[start] === newStr[end]) {
+      console.log("3");
+      start++;
+      end--;
+    } else {
+      console.log("4");
+      return false;
+    }
+  }
   return true;
 }
 
