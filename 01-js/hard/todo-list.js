9cbefa84c22b3614b880a66740307625330aca48
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-07-04 16:23:47 +0530

    todo-list done with all test cases

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,58 @@
 */
 
 class Todo {
+  constructor() {
+    this.todoList = [];
+  }
 
+  add(todo) {
+    this.todoList.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todoList.length) {
+      this.todoList.splice(indexOfTodo, 1);
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todoList.length) {
+      this.todoList[index] = updatedTodo;
+    }
+  }
+
+  getAll() {
+    return this.todoList;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todoList.length) {
+      return this.todoList[indexOfTodo];
+    }
+    return null;
+  }
+
+  clear() {
+    this.todoList.length = 0;
+  }
 }
+
+let newList = new Todo();
+newList.add("1");
+newList.add("2");
+newList.add("3");
+newList.add("4");
+newList.add("5");
+newList.add("6");
+newList.add("7");
+console.log(newList.todoList);
+newList.remove(0);
+console.log(newList.todoList);
+newList.update(1, "Updated");
+console.log(newList.todoList);
+console.log(newList.getAll());
+console.log(newList.get(2));
+newList.clear();
+console.log(newList.todoList);
 
 module.exports = Todo;
