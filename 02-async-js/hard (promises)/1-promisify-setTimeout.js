702eb7c830e60f365a2a49fdedfadd47ea319a4a
joy <joychoudhury318@gmail.com>
2023-07-05 22:24:14 +0530

    async-JS

--- 02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -7,3 +7,18 @@ function wait(n) {
         setTimeout(res, n * 1000);
     })
 }
+
+wait(2).then(() => {
+    console.log("Wait over for 2s");
+})
+
+wait(5).then(() => {
+    console.log("Wait over for 5s");
+})
+
+wait(10).then(() => {
+    console.log("Wait over for 10s");
+})
+wait(15).then(() => {
+    console.log("Wait over for 15s");
+})
--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -4,19 +4,36 @@
  * Print out the time it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
+function wait(n) {
+    return new Promise((res) => {
+        setTimeout(res, n * 1000)
+    })
+}
 
 function waitOneSecond() {
-
+    return wait(1);
 }
 
 function waitTwoSecond() {
-
+    return wait(2);
 }
 
 function waitThreeSecond() {
-
+    return wait(3);
 }
 
 function calculateTime() {
-
+    const ts = Date.now();
+    waitOneSecond()
+        .then(() => waitTwoSecond())
+        .then(() => waitThreeSecond())
+        .then(() => {
+            const tsElapsed = Date.now() - ts;
+            console.log(`Sequential time elapsed ${tsElapsed / 1000}s`)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
+calculateTime();
+
