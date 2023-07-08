8eb68d6fdb435cf43edc3d39175a0fc4ecee8175
Rasimul <rasimul29928@gmail.com>
2023-07-09 00:31:08 +0530

    Revert "expenditure_analysis solved"
    This reverts commit 20b394f88310be2074889385659db0526a672945.

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,36 +9,7 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  let result = [];
-  let categorySet = new Set();
-  for (let transaction of transactions) {
-    let { category, price } = transaction;
-    if (categorySet.has(category)) {
-      result.forEach(item => {
-        if (item.category === category) {
-          item.totalSpent += price;
-        }
-      })
-    } else {
-      categorySet.add(category);
-      let obj = {};
-      obj.category = category;
-      obj.totalSpent = price;
-      result.push(obj);
-    }
-  }
-  return result;
+  return [];
 }
 
-const transactions = [
-  {
-    id: 1,
-    timestamp: 1656076800000,
-    price: 10,
-    category: 'Food',
-    itemName: 'Pizza',
-  },
-];
-
-console.log(calculateTotalSpentByCategory(transactions));
 module.exports = calculateTotalSpentByCategory;
