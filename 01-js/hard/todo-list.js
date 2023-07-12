0a801b663ab65e5deeff8f4e22eb93c411ae9083
Ruchir-BridgeLabz <ruchir.dixit@bridgelabz.com>
2023-07-12 19:45:16 +0530

    Solved ToDoList hard problem for 01-JS Week1

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,46 @@
 */
 
 class Todo {
+  
+  constructor(){
+    this.toDoList = []
+  }
 
+  add(params) {
+    this.toDoList.push(params)
+  }
+
+  get(index){
+    if(index>this.toDoList.length-1){
+      return null
+    }
+    else{
+      return this.toDoList[index]
+    }
+  }
+
+  getAll(){
+    return this.toDoList
+  }
+
+  remove(index){
+    if (index > -1) { 
+      this.toDoList.splice(index, 1);
+    }
+  }
+
+  update(index,toUpdate){
+    if(index>this.toDoList.length-1){
+      return this.toDoList
+    }
+    else{
+      this.toDoList[index]=toUpdate
+    }
+  }
+
+  clear(){
+    this.toDoList = []
+  }
 }
 
 module.exports = Todo;
