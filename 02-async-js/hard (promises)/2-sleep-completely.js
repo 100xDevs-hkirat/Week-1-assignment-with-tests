b238d1e19d2f00cf1e90c4d43af000f512e991b5
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-14 13:22:39 +0530

    hard-promises-2

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,13 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds) {
+	const startTime = Date.now();
+	while (Date.now() - startTime < seconds * 1000) {
+		// Busy waiting loop
+		console.log("Hello World");
+	}
+	console.log("Out of scope"); //Executes only after the above loop, which we dont want.
 }
+
+sleep(5);
