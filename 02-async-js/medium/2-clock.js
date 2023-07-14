0f90db2ea293fc0bbe6ef17e6f457cbf1201c71d
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-14 12:28:11 +0530

    01-hard

--- 02-async-js/medium/2-clock.js
+++ 02-async-js/medium/2-clock.js
@@ -1,12 +1,14 @@
 function getTime() {
 	const now = new Date();
-	const hours = now.getHours().toString().padStart(2, "0");
-	const minutes = now.getMinutes().toString().padStart(2, "0");
-	const seconds = now.getSeconds().toString().padStart(2, "0");
+	const hours = now.getHours().toString().padStart(2, "0"); //This is to get atleast 2 digit number at all times.
+	const minutes = now.getMinutes().toString().padStart(2, "0"); //Same as above
+	const seconds = now.getSeconds().toString().padStart(2, "0"); //Same as above
 
-	const time12HourFormat = hours > 12 ? hours - 12 : hours;
+	const time12HourFormat = hours > 12 ? hours - 12 : hours; //Logic for a 12-hour fomrmat.
+	//Here we check if the 12hourformat number is greater than 12, if so then we minus the number with 12.
 	const amPm = hours >= 12 ? "PM" : "AM";
-	const formattedTime12Hour = `${time12HourFormat}:${minutes}:${seconds} ${amPm}`;
+	//If 12hourformatnumber > 12 then PM.
+	const formattedTime12Hour = `${time12HourFormat}:${minutes}:${seconds} ${amPm}`; //DYnamically adding the values .
 
 	const formattedTime24Hour = `${hours}:${minutes}:${seconds}`;
 
