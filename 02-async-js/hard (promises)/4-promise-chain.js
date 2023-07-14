de895bc52918ed8516f082880415b442bcbe15ab
devanshuc <66375026+devanshuc@users.noreply.github.com>
2023-07-14 13:36:26 +0530

    hard-3&4. Promise All and Promise Chain added. Great assignment to demonstrate how promises work

--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -5,18 +5,47 @@
  * Compare it with the results from 3-promise-all.js
  */
 
-function waitOneSecond() {
-
+function waitOneSecond(n) {
+	return new Promise((resolve) => {
+		console.log("Will be resolved in 1 second");
+		setTimeout(() => {
+			resolve();
+			console.log("1 second Done");
+		}, n * 1000);
+	});
 }
 
-function waitTwoSecond() {
-
+function waitTwoSecond(n) {
+	return new Promise((resolve) => {
+		console.log("Will be resolved in 2 second");
+		setTimeout(() => {
+			resolve();
+			console.log("2 second Done");
+		}, n * 1000);
+	});
 }
 
-function waitThreeSecond() {
-
+function waitThreeSecond(n) {
+	return new Promise((resolve) => {
+		console.log("Will be resolved in 3 second");
+		setTimeout(() => {
+			resolve();
+			console.log("3 second Done");
+		}, n * 1000);
+	});
 }
 
 function calculateTime() {
+	const startTime = Date.now();
 
+	return waitOneSecond(1)
+		.then(() => waitTwoSecond(2))
+		.then(() => waitThreeSecond(3))
+		.then(() => {
+			const endTime = Date.now();
+			const duration = endTime - startTime;
+			console.log(`Sequential promises completed in ${duration} milliseconds`);
+		});
 }
+
+calculateTime();
