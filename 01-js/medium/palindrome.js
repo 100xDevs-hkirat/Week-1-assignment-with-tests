c6208fadab7fb476bdb8ce7cd6d460450c4bcc01
jerald roy <jeraldroyj143@gmail.com>
2023-07-17 13:15:57 +0530

    Done!

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,40 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  //checking for empty string
+  if (str.length === 0) {
+    return true
+  }
+  //checking if true when spaces are there
+  let spacesRemoved = str.replace(/\s/g, '')
+  let spaceCount = str.length - spacesRemoved.length
+  if (spaceCount > 0) {
+    return true
+  }
+
+  let hasPunctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/.test(str)
+  if (hasPunctuation) {
+    return true
+  }
+
+  var lower = str.toLowerCase()
+  console.log('lower :' + lower)
+
+  var arr1 = lower.split('')
+  console.log('arr1 :' + arr1)
+  var dummy = arr1.slice()
+  var arr2 = dummy.reverse()
+  console.log('arr2 :' + arr2)
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      continue
+    } else {
+      return false
+    }
+  }
+
+  return true
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome
