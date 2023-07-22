5ec884d54d7cd51217e1c0d13d33da0cd96b0c1b
Rudra <rudrakshnile930@gmail.com>
2023-07-22 13:16:54 +0530

    Easy Medium & Hard All DONE

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,43 @@
 */
 
 class Todo {
+  constructor() {
+    this.arr = new Array();
+  }
 
+  add(todo) {
+    this.arr.push(todo);
+  }
+
+  remove(index) {
+    for (let i = index; i < this.arr.length; i++) {
+      this.arr[i] = this.arr[i + 1];
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index >= this.arr.length) {
+      return;
+    } else {
+      this.arr[index] = updatedTodo;
+    }
+  }
+
+  getAll() {
+    return this.arr;
+  }
+
+  get(indexOfTodo) {
+    if (indexOfTodo >= this.arr.length) {
+      return null;
+    } else {
+      return this.arr[indexOfTodo];
+    }
+  }
+
+  clear() {
+    this.arr = new Array();
+  }
 }
 
 module.exports = Todo;
