fb260ae613027761bcc480aa87d9c1a42f9e3a59
allakattu <sairam.allakattu@phenompeople.com>
2023-07-29 01:57:05 +0530

    testing git

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -3,7 +3,7 @@
   What's Anagram?
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 
-  Once you've implemented the logic, test your code by running
+  Once you've implemented the logic, test your code by running.
   - `npm run test-anagram`
 */
 
