91fc7a1a864187e030736e4db5da7e5083f83216
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-08-06 17:46:52 +0530

    completing array map function

--- extra-assingments/arrayMap.js
+++ extra-assingments/arrayMap.js
@@ -1,3 +1,17 @@
 function mapArray(arr, callback) {
-    
+    function xyz(element, index, array) {
+        return callback(arr[index]);
+    }
+
+    return arr.map(xyz);
 }
+
+testArr = [1, 2, 3, 4];
+function add1(element) {
+    return element = 1;
+
+}
+
+newArr = mapArray(tesstArr, add1)
+console.log(newArr);
+// Excpect : newArr = [2, 3, 4, 5]
