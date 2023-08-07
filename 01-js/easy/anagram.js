c74903b0ebbbcea5bd04d8ffe73cb2f9ceffe9b3
niyardutta <cyberhacker278@gmail.com>
2023-08-08 02:26:11 +0530

    anagram solution added

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,24 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
-
+function sort(str) {
+  var array = str.split("");
+  array = array.sort();
+  var sortedString = array.join("");
+  return sortedString;
 }
 
+
+function isAnagram(str1, str2) {
+  var str1Lowercase = str1.toLowerCase();
+  var str2Lowercase = str2.toLowerCase();
+  if (sort(str1Lowercase) == sort(str2Lowercase)) {
+    return true;
+   } else {
+    return false;
+  }
+}
+
+
+var answer = isAnagram("bold", "obld")
 module.exports = isAnagram;
