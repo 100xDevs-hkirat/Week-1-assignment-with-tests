61bb7e89e3deb20f0382bd3826f2e5be2dd52e41
Paras Nagpal <paras.nagpal743@gmail.com>
2023-08-12 08:07:52 +0530

    Added solution of anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,6 +8,24 @@
 */
 
 function isAnagram(str1, str2) {
+         if(str1.length !== str2.length) return false ;
+         var dict1 = {} ;
+         var dict2 = {} ;
+         str1 = str1.toLowerCase() ;
+         str2 = str2.toLowerCase() ;
+
+         for(var i = 0 ; i < str1.length ; ++i){
+              if(str1[i] in dict1) dict1[str1[i]] += 1 ;
+              else dict1[str1[i]] = 1 ;
+              if(str2[i] in dict2) dict2[str2[i]] += 1 ;
+              else dict2[str2[i]] = 1 
+         }
+       
+         for(var key in dict1){
+            if(dict1[key] !== dict2[key]) return false ;
+         }
+
+         return true ;
 
 }
 
