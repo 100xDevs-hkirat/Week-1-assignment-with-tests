26dabda892058c75b14ae399490c26e03c878f2a
meghanabt <meghana.b.thirumala@gmail.com>
2023-08-12 18:13:46 -0700

    o1-js changes

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,10 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    var sum=0
+    let startTime= new Date().getTime()
+    for(let i=0;i<n;i++) sum+=i
+    let endTime= new Date().getTime()
+
+    return (endTime-startTime)/1000;
 }
