22648f2e29ac42a022b9fbf0df70cbd8fd9366ed
Sameer <111224603+SameerVers3@users.noreply.github.com>
2023-08-13 17:17:47 -0700

    02-async-js hard promise-chain

--- 02-async-js/hard/4-promise-chain.js
+++ 02-async-js/hard/4-promise-chain.js
@@ -6,17 +6,40 @@
  */
 
 function waitOneSecond() {
-
+    return new Promise((resolve) => {
+        console.log("inside 1")
+        setTimeout(resolve, 1 * 1000);
+    })
 }
 
 function waitTwoSecond() {
-
+    return new Promise((resolve) => {
+        console.log("inside 2")
+        setTimeout(resolve, 2 * 1000);
+    })
 }
 
 function waitThreeSecond() {
-
+    return new Promise((resolve) => {
+        console.log("inside 3")
+        setTimeout(resolve, 3 * 1000);
+    })
 }
 
 function calculateTime() {
-
+    let start = new Date();
+    waitOneSecond().then(() => {
+        console.log("1 completed")
+        waitTwoSecond().then(()=>{
+            console.log("2 completed")
+            waitThreeSecond().then(()=>{
+                console.log("3 completed")
+                let end = new Date();
+                let time = (end.getTime() - start.getTime()) / 1000;
+                console.log("Time to resolve : " + time + " s");
+            })
+        })
+    })
 }
+
+calculateTime();
