69092b54e92529c41f02c6f4d562fe5d90677a13
Dev Harwani <harwanidev@gmail.com>
2023-08-20 01:47:08 +0530

    solved anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,65 @@
 */
 
 function isAnagram(str1, str2) {
+str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
 
+    
+
+    str1_array = [];
+    str2_array = [];
+    
+
+    for (var i =0; i<str1.length; i++){
+        if (str1[i] == " "){
+            continue;
+        }
+
+        str1_array .push(str1[i]);
+    }
+
+    for (var i =0; i<str2.length; i++){
+        if (str2[i] == " "){
+            continue;
+        }
+
+        str2_array .push(str2[i]);
+    }
+
+    
+    for (var i =0; i<str2.length; i++){
+        str2_array.push(str2[i]);
+    }
+
+    for (var i = 0; i<str1.length-1; i++){
+        for (var j=0; j<str1.length-1-i; j++){
+            if (str1_array[j] > str1_array[j+1]){
+                var temp = str1_array[j];
+                str1_array[j] = str1_array[j+1];
+                str1_array[j+1] = temp;
+            }
+        }
+    }
+    
+    for (var i = 0; i<str2.length-1; i++){
+        for (var j=0; j<str2.length-1-i; j++){
+            if (str2_array[j] > str2_array[j+1]){
+                var temp = str2_array[j];
+                str2_array[j] = str2_array[j+1];
+                str2_array[j+1] = temp;
+            }
+        }
+    }
+    
+
+    for (var i =0; i<str1_array.length; i++){
+        if (str1_array[i] != str2_array[i]){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
 
 module.exports = isAnagram;
