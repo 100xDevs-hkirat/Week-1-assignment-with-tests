2ebffb93e32192c8d60d14a8b75a5a18dd37f8ff
maheshsp <maheshsp1809@gmail.com>
2023-09-11 22:13:16 +0530

    checking to merge from main to main

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,14 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(milliseconds) {
+  const start = Date.now();
+  while (Date.now() - start < milliseconds) {
+    // Busy-wait until the specified time has passed
+  }
 }
+console.log("Before sleep");
+
+sleep(2000); // Sleep for 2000 milliseconds (2 seconds)
+
+console.log("After sleep");
