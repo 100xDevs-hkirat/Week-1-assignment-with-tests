66beac964969460f73624f7ce17e56d297e3f01e
Nitin Chandani <nitin1chandani@gmail.com>
2023-09-24 18:32:57 +0530

    easy done, medium 1st is done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,10 @@
 */
 
 function isAnagram(str1, str2) {
-
+  if (str1.split("").sort().join("") == str2.split("").sort().join("")) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isAnagram;
