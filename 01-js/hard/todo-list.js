184f826c38c8af76a5610ab545fb538cd72677b3
sahil <sahilcodes23@gmail.com>
2023-10-31 16:42:12 +0530

    latest commit

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,35 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.todol = [];
+  }
+  add(task) {
+    this.todol.push(task);
+  }
+  getAll() {
+    return this.todol;
+  }
+  remove(number) {
+    this.todol.splice(number, 1);
+  }
+  update(numtask, taskk) {
+    if (numtask >= this.todol.length) {
+      return this.todol;
+    }
+    this.todol[numtask] = taskk;
+    return this.todol;
+  }
+  get(numtask) {
+    if (numtask >= this.todol.length) {
+      return null;
+    }
+    return this.todol[numtask];
+  }
+  clear() {
+    this.todol = [];
+    return this.todol;
+  }
 }
 
 module.exports = Todo;
