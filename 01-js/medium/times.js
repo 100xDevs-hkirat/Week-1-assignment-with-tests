bd510236aa1b9c4ba0e4d81ce1c2f5d298d7d0d4
Yash Rai <82558647+yashrai652@users.noreply.github.com>
2023-11-04 12:06:49 +0530

    Update times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -1,12 +1,12 @@
-/*
-Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
-Try running it for
-1. Sum from 1-100
-2. Sum from 1-100000
-3. Sum from 1-1000000000
-Hint - use Date class exposed in JS
-*/
+
 
 function calculateTime(n) {
-    return 0.01;
+     var startTime=performance.now();
+    var sum=0;
+    for(var i=1;i<=n;i++){
+        sum+=i;
+    }
+    var endTime=performance.now();
+    var timePassed=(endTime-startTime)/1000;
+    return timePassed;    
 }
