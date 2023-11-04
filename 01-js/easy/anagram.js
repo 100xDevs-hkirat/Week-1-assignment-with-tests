1257e51a724982f67bd7a54df1fa3e569fe92dfe
Yash Rai <82558647+yashrai652@users.noreply.github.com>
2023-11-04 09:45:26 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -1,14 +1,14 @@
-/*
-  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
-  What's Anagram?
-  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 
-  Once you've implemented the logic, test your code by running
-  - `npm run test-anagram`
-*/
 
 function isAnagram(str1, str2) {
-
+ string1=str1.toLowerCase();
+ string2=str2.toLowerCase();
+    if(string1.length!==string2.length)
+      return false;
+    if(string1.split('').sort().join()==string2.split('').sort().join())
+      return true;
+    else 
+      return false;
 }
 
 module.exports = isAnagram;
