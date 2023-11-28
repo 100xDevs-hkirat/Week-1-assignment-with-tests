84ff683d96d47524de0e85c738337fa330b4bb98
priyanshu8346 <91590140+priyanshu8346@users.noreply.github.com>
2023-11-28 16:32:51 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,19 @@
 */
 
 function isAnagram(str1, str2) {
-
+let obj = new Object();
+  for(let i = 0; i < str1.length; i++){
+    if(obj[str1[i]]) obj[str1[i]]++;
+    else obj[str1[i]] = 1;
+  }
+  for(let i = 0; i < str2.length; i++){
+    if(obj[str1[i]]) obj[str2[i]]--;   
+    else return false;    
+  }
+    for(let key in obj){
+      if(obj[key] !== 0)return false;
+    }
+  return true;
 }
 
 module.exports = isAnagram;
