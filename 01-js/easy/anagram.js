07b3ae68730f305cbd826a4aef94f56e5ba5d27e
VinayPolisetti <97217908+VinayPolisetti@users.noreply.github.com>
2024-01-13 11:39:21 +0530

    Question 1 solved

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,44 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
+  let freq1 = Array(26).fill(0);
+  let freq2 = Array(26).fill(0);
+
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] === ' ') {
+      continue;
+    }
+    if (str1[i] >= 'A' && str1[i] <= 'Z') {
+      freq1[str1[i].charCodeAt(0) - 'A'.charCodeAt(0)]++;
+    } else {
+      freq1[str1[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+  }
+
+  for (let i = 0; i < str2.length; i++) {
+    if (str2[i] === ' ') {
+      continue;
+    }
+    if (str2[i] >= 'A' && str2[i] <= 'Z') {
+      freq2[str2[i].charCodeAt(0) - 'A'.charCodeAt(0)]++;
+    } else {
+      freq2[str2[i].charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+  }
+
+  for (let i = 0; i < 26; i++) {
+    if (freq1[i] !== freq2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
+// console.log(isAnagram("abc", "bac"));
 module.exports = isAnagram;
+
